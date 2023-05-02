58f15d60aebd73c60ab06fd07b5d5f997e6c8304
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-05-01 22:02:27 -0400

    Fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -1,10 +1,11 @@
 // test/posts.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { describe, it } = require('mocha');
+const { describe, it, after } = require('mocha');
 chai.use(chaiHttp);
 const app = require('../server');
 const agent = chai.request.agent(app);
+const User = require('../models/user');
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
@@ -13,12 +14,32 @@ const Post = require('../models/post');
 const should = chai.should();
 
 describe('Posts', () => {
-  // Post that we'll use for testing purposes
+    // Post that we'll use for testing purposes
     const newPost = {
         title: 'post title',
         url: 'https://www.google.com',
-        summary: 'post summary'
+        summary: 'post summary',
+        subreddit: 'website'
     };
+
+    const user = {
+        username: 'poststest',
+        password: 'testposts'
+    };
+
+    before(function (done) {
+        agent
+            .post('/sign-up')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(user)
+            .then(function (res) {
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+
     it('should create with valid attributes at POST /posts/new', (done) => {
         // Checks how many posts there are now
         Post.estimatedDocumentCount()
@@ -52,8 +73,24 @@ describe('Posts', () => {
             });
     });
 
+    after(function (done) {
+        Post.findOneAndDelete(newPost)
+            .then(function () {
+                agent.close();
 
-    after(() => {
-        Post.findOneAndDelete(newPost);
+                User
+                    .findOneAndDelete({
+                        username: user.username,
+                    })
+                    .then(function () {
+                        done();
+                    })
+                    .catch(function (err) {
+                        done(err);
+                    });
+            })
+            .catch(function (err) {
+                done(err);
+            });
     });
 });
